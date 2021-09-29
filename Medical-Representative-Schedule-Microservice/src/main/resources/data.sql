00aacd56c3275b092c8eba20a3077a223afbcf1e
Singhaniya Aman <78849123+singhaniya1216@users.noreply.github.com>
2021-09-29 16:09:12 +0530

    Delete Medical-Representative-Schedule-Microservice directory

--- Medical-Representative-Schedule-Microservice/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into medical_representatives (name, phone_number) values('SinghaniyaAman', '9876543210');
-insert into medical_representatives (name, phone_number) values('DeepakParmar', '9876543211');
-insert into medical_representatives (name, phone_number) values('SanakaPradeep', '9876543212');
-insert into medical_representatives (name, phone_number) values('Shushma', '9876543213');
